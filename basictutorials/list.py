b02e17a05466a0bbd66f904a6fbd8b08bdbee64d
Gimun Lee <gimunlee@kaist.ac.kr>
2017-05-11 15:15:52 +0900

    generator

--- basictutorials/list.py
+++ basictutorials/list.py
@@ -1,19 +1,43 @@
-""" list tutorials """
+""" List tutorials """
+def main():
+    """Test code"""
+    li = [3, 2, 6]
+    tail_li = [4, 5]
 
-LI = [3, 2, 6]
-TAIL_LI = [4, 5]
+    #li.append(tailli)
+    li.extend(tail_li)
 
-#li.append(tailLi)
-LI.extend(TAIL_LI)
+    print li
+    help(len)
+    print dir(list)
+    help(list.__add__)
 
-print LI
-help(len)
-print dir(list)
-help(list.__add__)
+    print sorted(li)
+    print li
+    li.reverse()
+    print li
+    li.sort()
+    print li
 
-print sorted(LI)
-print LI
-LI.reverse()
-print LI
-LI.sort()
-print LI
+    #List comprehension
+    wood = 'How much wood would a woodchuck chuck if a woodchuck could chuck wood?'.split()
+    print wood
+    print [list(x) for x in wood if len(x) == 5]
+
+    #Generator ~~ lazy evaluation
+    def generator(size):
+        """sample generator"""
+        i = 0
+        while i < size:
+            print 'i = %d' % i
+            yield i
+            i += 1
+    for step in generator(5):
+        print 'step = %d' % step
+
+    #Generator expression
+    tuple1 = tuple((x + 1 for x in range(3)))
+    print tuple1
+
+if __name__ == "__main__":
+    main()
