420b8e5d216172ba5227d59171a73cf9d9dbe0e1
Gimun Lee <gimunlee@kaist.ac.kr>
2017-05-11 11:47:41 +0900

    dictionary

---
+++ basictutorials/dictionarytutorial.py
@@ -0,0 +1,15 @@
+""" dictionary tutorial """
+SIMPSONS = {
+    'Homer': 36,
+    'Marge': 35,
+    'Bart': 10
+}
+SIMPSONS['Lisa'] = 8
+print SIMPSONS
+
+SIMPSONS['Marge'] = 36
+print SIMPSONS
+
+print SIMPSONS.keys()
+print SIMPSONS.values()
+print SIMPSONS.items()
