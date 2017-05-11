86a8a33e348ccbfc2ccb0dfe86893d49152baacd
Gimun Lee <gimunlee@kaist.ac.kr>
2017-05-11 15:15:46 +0900

    pickle advanced

--- basictutorials/pickletuto.py
+++ basictutorials/pickletuto.py
@@ -1,10 +1,31 @@
 """ Pickling tutorial """
 import pickle
+import cPickle
 
-T = [1, 2, 3]
-REPR_T = pickle.dumps(T)
-T2 = pickle.loads(REPR_T)
-print T2
+def main():
+    """Test code"""
 
-print T == T2
-print T is T2
+    temp_list = [1, 2, 3]
+    repr_temp_list = pickle.dumps(temp_list)
+    temp_list2 = pickle.loads(repr_temp_list)
+    print temp_list2
+
+    print temp_list == temp_list2
+    print temp_list is temp_list2
+
+    grades = [3, 2, 1]
+    grades_file = open('grades.dat', 'wb')
+    pickle.dump(grades, grades_file, -1)
+    grades_file.close()
+
+    new_grades_file = open('grades.dat', 'rb')
+    new_grades = pickle.load(new_grades_file)
+    new_grades_file.close()
+    print new_grades
+
+    repr_temp_list = cPickle.dumps(temp_list)
+    temp_list2 = cPickle.loads(repr_temp_list)
+    print temp_list2
+
+if __name__ == '__main__':
+    main()
