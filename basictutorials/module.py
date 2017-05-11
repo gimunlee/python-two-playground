eeb436edfde2bfa61766a14517e40bd5d88e9974
Gimun Lee <gimunlee@kaist.ac.kr>
2017-05-11 13:30:01 +0900

    module

---
+++ basictutorials/module.py
@@ -0,0 +1,6 @@
+""" Tutorial for modules """
+
+print 'In module.py, __name__ is %s' % __name__
+
+import tutomodule
+tutomodule.test_add(5, 4)
---
+++ basictutorials/tutomodule.py
@@ -0,0 +1,11 @@
+""" The module script """
+
+print 'In tutomodule.py, __name__ is %s' % __name__
+
+#test module
+
+def test_print():
+    print 'you called this'
+
+def test_add(x, y):
+    print x + y
